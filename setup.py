8124403d1193192c20ea41aa822fddf15833b1f7
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2016-10-30 09:50:35 -0400

    Added more keywords and strings classifiers items for the PyPi metadata package

--- setup.py
+++ setup.py
@@ -3,18 +3,29 @@ import os
 
 setup(
     name='plonetheme.earthlingtwo',
-    version='0.2',
-    description='An installable Diazo theme for Plone 4.1',
+    version='0.3',
+    description='An installable Diazo theme for earthlingtwo Theme at Plone 4.x',
     long_description=open("README.rst", "rb").read() +
                      open(os.path.join("docs", "HISTORY.txt"), "rb").read(),
+    # Get more strings from
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Zope2',
+        'Framework :: Zope3',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='web zope plone theme diazo',
     author='Andrew Mleczko',
     author_email='amleczko@redturtle.it',
     url='http://pypi.python.org/pypi/plonetheme.earthlingtwo',
-    classifiers=[
-        'Framework :: Plone',
-        'Programming Language :: Python',
-    ],
-    keywords='web zope plone theme diazo',
     packages=find_packages(),
     include_package_data=True,
     namespace_packages=[
